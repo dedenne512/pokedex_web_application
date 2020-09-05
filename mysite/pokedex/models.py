78bba6f839da9db4fa99be42427a23ec5ce7070b
yu0512 <chelsea100512@gmail.com>
2020-09-05 10:04:27 +0900

    ok

---
+++ mysite/pokedex/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+class Pokemon(models.Model):
+    number = models.IntegerField('図鑑ナンバー')
+    name_jpn = models.CharField('名前', max_length=255)
+    name_eng = models.CharField('名前(英語)', max_length=255)
+    classification = models.CharField('分類', max_length=255)
+    type1 = models.CharField('タイプ1', max_length=255)
+    type2 = models.CharField('タイプ2', null=True, max_length=255)
+    height = models.DecimalField('高さ', max_digits=4, decimal_places=1)
+    weight = models.DecimalField('重さ', max_digits=4, decimal_places=1)
+    ability1 = models.CharField('特性1', max_length=255)
+    ability2 = models.CharField('特性2', null=True, max_length=255)
+    hidden_ability = models.CharField('夢特性', null=True, max_length=255)
+    detail = models.TextField('図鑑説明', max_length=500)
+    image = models.FilePathField('画像', null=True)
+    voice = models.FilePathField('音声', null=True)
+
